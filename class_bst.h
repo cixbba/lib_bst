aef88b1eb8c4b7ba6b55f024e87fe96c2023a923
cixbba <ciobanu194@gmail.com>
2024-11-25 20:49:24 +0100

    bst implementato con una classe

---
+++ class_bst.h
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+private:
+    int data;
+    int weight;
+    Node* lchild;
+    Node* rchild;
+
+public:
+    // Costruttore
+    Node(int v) : data(v), weight(1), lchild(nullptr), rchild(nullptr) {}
+
+    // Metodo per inserimento ricorsivo
+    Node* insertR(int k) {
+        if (this == nullptr) {
+            return new Node(k);
+        }
+        if (k == data) {
+            weight++;
+            return this;
+        }
+        if (k < data) {
+            if (lchild == nullptr) {
+                lchild = new Node(k);
+            } else {
+                lchild = lchild->insertR(k);
+            }
+        } else {
+            if (rchild == nullptr) {
+                rchild = new Node(k);
+            } else {
+                rchild = rchild->insertR(k);
+            }
+        }
+        return this;
+    }
+
+    // Metodo per attraversamento in inOrder
+    void inOrder() {
+        if (lchild != nullptr) {
+            lchild->inOrder();
+        }
+        cout << data << " ";
+        if (rchild != nullptr) {
+            rchild->inOrder();
+        }
+    }
+
+    int getData() { return data; }
+    int getWeight() { return weight; }
+    Node* getLeftChild() { return lchild; }
+    Node* getRightChild() { return rchild; }
+
+    void setLeftChild(Node* child) { lchild = child; }
+    void setRightChild(Node* child) { rchild = child; }
+};
